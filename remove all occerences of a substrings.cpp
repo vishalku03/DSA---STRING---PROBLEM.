8927228956a42843382f30467b60e8334a5d71c0
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-01-27 04:20:26 +0530

    Add files via upload

---
+++ remove all occerences of a substrings.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+     string removeOccurrences(string s, string part)
+     {
+
+          while (s.find(part) != string::npos)
+          {
+               s.erase(s.find(part), part.length());
+          }
+
+          return s;
+     }
+};
