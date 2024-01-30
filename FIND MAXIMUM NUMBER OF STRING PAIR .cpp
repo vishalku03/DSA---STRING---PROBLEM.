d9cbe05732cc3c315bbacfb240d69d3a3c5a6dd0
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-01-30 21:48:26 +0530

    Add files via upload

---
+++ FIND MAXIMUM NUMBER OF STRING PAIR .cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int maximumNumberOfStringPairs(vector<string> &words)
+     {
+          int ans = 0;
+          unordered_map<string, int> mp;
+          for (auto w : words)
+          {
+               string r = w;
+               reverse(r.begin(), r.end());
+               if (mp[r] > 0)
+               {
+                    ans++;
+                    mp[r]--;
+               }
+               else
+                    mp[w]++;
+          }
+          return ans;
+     }
+};
+
+// LEETCODE QUESTION - 2744
