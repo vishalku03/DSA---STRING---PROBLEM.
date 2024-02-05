85988e6f575001baa32227d32ac579a94bcc4d3b
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-05 21:22:19 +0530

    Add files via upload

---
+++ ROMAN NUMBER TO INTEGER.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int romanToDecimal(string &str)
+     {
+
+          int n = str.size();
+
+          map<char, int> m;
+          m['I'] = 1;
+          m['V'] = 5;
+          m['X'] = 10;
+          m['L'] = 50;
+          m['C'] = 100;
+          m['D'] = 500;
+          m['M'] = 1000;
+
+          int ans = 0;
+
+          for (int i = 0; i < str.length(); i++)
+          {
+               if (m[str[i]] < m[str[i + 1]])
+               {
+                    ans = ans - m[str[i]];
+               }
+               else
+               {
+                    ans = ans + m[str[i]];
+               }
+          }
+          return ans;
+     }
+};
+
+//GFG
