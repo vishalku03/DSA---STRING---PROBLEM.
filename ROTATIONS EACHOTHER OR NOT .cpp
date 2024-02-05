2eccbe6d266a4c11fec9196a2227c8e8ad94dc72
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-05 21:58:04 +0530

    Add files via upload

---
+++ ROTATIONS EACHOTHER OR NOT .cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     // Function to check if two strings are rotations of each other or not.
+     bool areRotations(string s1, string s2)
+     {
+          // Your code here
+          if (s1.length() != s2.length())
+               return false;
+
+          string ans = s1 + s1;
+
+          return (ans.find(s2) != -1);
+     }
+};
+
+// GFG
