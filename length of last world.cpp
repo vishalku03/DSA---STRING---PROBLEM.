e92f12e716313ed3b5a5ea6ae8336c347672a5d4
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-04-01 10:35:26 +0530

    Add files via upload

---
+++ length of last world.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+     int lengthOfLastWord(string s)
+     {
+
+          int n = s.length();
+          int i = n - 1;
+
+          while (i >= 0 && s[i] == ' ')
+               i--;
+
+          int length = 0;
+          while (i >= 0 && s[i] != ' ')
+          {
+               i--;
+               length++;
+          }
+          return length;
+     }
+};
+
+// leetcode -  58
